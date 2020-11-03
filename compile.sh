47504ffd729a77f50017040ebc9271a17ba9a5e4
Sven Pachnit <sven@bmonkeys.net>
2020-11-03 13:22:36 +0100

    Update compile script
    where is my old scripting folder? :<

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd addons/sourcemod/scripting
 set -e
+SDK_PATH="/Users/chaos/Downloads/sourcemod-1.10.0-git6499-mac/addons/sourcemod/scripting"
 
 for SPFILE in gungame gungame_afk gungame_bot gungame_config gungame_display_winner gungame_logging gungame_mapvoting gungame_mvp gungame_one_shot_awp gungame_stats gungame_tk gungame_warmup_configs gungame_winner_effects
 do
@@ -8,6 +9,6 @@ do
   echo "##### $SPFILE"
   echo "#####################"
   if [ -f ../plugins/$SPFILE.smx ]; then rm ../plugins/$SPFILE.smx; fi
-  ~/Downloads/sourcemod_scripting/spcomp -i /Users/chaos/Downloads/sourcemod_scripting/include -iinclude $SPFILE.sp WITH_SDKHOOKS=1
+  $SDK_PATH/spcomp -i $SDK_PATH/include -iinclude $SPFILE.sp WITH_SDKHOOKS=1
   mv $SPFILE.smx ../plugins/
 done
